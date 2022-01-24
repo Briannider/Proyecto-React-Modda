5065d6207611fb556be1e5bc708e93df20d38c20
Briannider <braianniderhaus@gmail.com>
2022-01-24 17:28:07 -0300

    Cambios en metatags y solucion de iconpolitics

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -10,17 +10,17 @@ const Footer = () => {
                 <div className="col-7">
                     <ul className="d-grid text-start p-0 list-unstyled">
                         <li><a href="#" target="_blank"><img src="img/contact.png" className="p-2" alt="Contact" />Contacto</a></li>
-                        <li><a href="#" target="_blank"><img src="img/redes.png" className="p-2" alt="Socialmedias" />Redes</a></li>
-                        <li><a href="#" target="_blank"><img src="img/aboutus.png" className="p-2" alt="Aboutus" />Sobre Nosotros</a></li>
+                        <li><a href="#" target="_blank"><img src="img/redes.png" className="p-2" alt="Social_medias" />Redes</a></li>
+                        <li><a href="#" target="_blank"><img src="img/aboutus.png" className="p-2" alt="About_us" />Sobre Nosotros</a></li>
                         <li><a href="#" target="_blank"><img src="img/Politics.png" className="p-2" alt="Politics_icon" />Politicas y privacidad</a></li>
                     </ul>
                 </div>
                 <div className="col-5">
                     <ul className="d-grid text-end list-unstyled">
-                        <li><a href="#" target="_blank"><img src="img/facebook.png" className="p-2" alt="iconwebfacebook" />Facebook</a></li>
-                        <li><a href="#" target="_blank"><img src="img/instagram.png" className="p-2" alt="iconwebinstagram" />Instagram</a></li>
-                        <li><a href="#" target="_blank"><img src="img/twitter.png" className="p-2" alt="iconwebtwitter" />Twitter_es</a></li>
-                        <li><a href="#" target="_blank"><img src="img/whatsapp.png" className="p-2" alt="iconwebwhatsapp" />Whatsapp</a></li>
+                        <li><a href="#" target="_blank"><img src="img/facebook.png" className="p-2" alt="icon_web_facebook" />Facebook</a></li>
+                        <li><a href="#" target="_blank"><img src="img/instagram.png" className="p-2" alt="icon_web_instagram" />Instagram</a></li>
+                        <li><a href="#" target="_blank"><img src="img/twitter.png" className="p-2" alt="icon_web_twitter" />Twitter_es</a></li>
+                        <li><a href="http://wa.me/541133425726/?text=contacto" target="_blank"><img src="img/whatsapp.png" className="p-2" alt="icon_web_whatsapp" />Whatsapp</a></li>
                     </ul>
                 </div>
             </footer>
